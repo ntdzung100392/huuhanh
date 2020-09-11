430934e1c64c7251388e8ec663f1a5f8f2a208c3
Dzung Nguyen Trung <dung.nt100392@gmail.com>
2020-09-11 15:10:22 +0700

    Update folder

--- src/WareHouseApps/Program.cs
+++ src/WareHouseApps/Program.cs
@@ -14,9 +14,6 @@ namespace WareHouseApps
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         private static void Main()
         {
